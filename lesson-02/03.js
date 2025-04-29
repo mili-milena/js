f4b677dfe6415efc86a4db3bae4c0a9518b60939
Milena <milenapavlovihc14@gmail.com>
2025-04-29 14:48:52 +0300

    lesson-02/03.2

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,18 +1,18 @@
-const score = 70; // тестовое значение, можно изменять
-let grade;
+const score = 70 // тестовое значение, можно изменять
+let grade
 
 if (score >= 90 && score <= 100) {
-    grade = 'A';
-} else if (score >= 80 && score < 90) {
-    grade = 'B';
-} else if (score >= 70 && score < 80) {
-    grade = 'C';
-} else if (score >= 50 && score < 70) {
-    grade = 'D';
-} else if (score >= 0 && score < 50) {
-    grade = 'F';
+  grade = 'A'
+} else if (score >= 80 && score <= 89) {
+  grade = 'B'
+} else if (score >= 70 && score <= 79) {
+  grade = 'C'
+} else if (score >= 50 && score <= 69) {
+  grade = 'D'
+} else if (score >= 0 && score <= 49) {
+  grade = 'F'
 } else {
-    grade = 'Неверный балл'; // на случай, если балл вне диапазона
+  grade = 'Invalid score' // обработка недопустимых значений
 }
 
-console.log(grade);
+console.log(`Score: ${score}, Grade: ${grade}`)
