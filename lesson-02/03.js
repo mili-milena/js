80e322576bc65f82de37debb32385e0e90db892c
Milena <milenapavlovihc14@gmail.com>
2025-04-29 14:52:55 +0300

    lesson-02/03.3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -2,17 +2,15 @@ const score = 70 // тестовое значение, можно изменят
 let grade
 
 if (score >= 90 && score <= 100) {
-  grade = 'A'
-} else if (score >= 80 && score <= 89) {
-  grade = 'B'
-} else if (score >= 70 && score <= 79) {
-  grade = 'C'
-} else if (score >= 50 && score <= 69) {
-  grade = 'D'
-} else if (score >= 0 && score <= 49) {
-  grade = 'F'
-} else {
-  grade = 'Invalid score' // обработка недопустимых значений
-}
-
-console.log(`Score: ${score}, Grade: ${grade}`)
+    grade = 'A';
+  } else if (score >= 80 && score <= 89) {
+    grade = 'B';
+  } else if (score >= 70 && score <= 79) {
+    grade = 'C';
+  } else if (score >= 50 && score <= 69) {
+    grade = 'D';
+  } else if (score >= 0 && score <= 49) {
+    grade = 'F';
+  } else {
+    grade = 'Invalid';
+  }
