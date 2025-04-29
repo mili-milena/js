411c750d7da6444fb97d7aa5a41259809b00ff4e
Milena <milenapavlovihc14@gmail.com>
2025-04-29 14:30:58 +0300

    lesson-02

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,18 +1,10 @@
-/*
-* Создать программный код, который определяет, может ли пользователь получить доступ к ресурсу на основе его роли и наличия разрешений.
-* Полученное булево значение сохраните в переменной isAccess.
+const isAdmin = false;
+const isVerifiedUser = true;
+const hasSpecialPermission = true;
+const hasTemporaryPass = false;
 
-* Условия доступа:
-* - Пользователь должен иметь статус администратора (isAdmin) или быть подтвержденным пользователем (isVerifiedUser)
-* - А также пользователь должен обладать специальным разрешением (hasSpecialPermission) или временным пропуском (hasTemporaryPass)
-*/
+let isAccess;
 
-// тестовые данные (значения можно менять)
-const isAdmin = false
-const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+isAccess = (isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass);
 
-let isAccess
-
-// your code
+console.log(isAccess);
--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,9 +1,18 @@
-/*
- * Напишите код, который будет определять оценку студента (grade) в зависимости от балла (score).
- * Балл может быть целым числом от 0 до 100, а оценки 'F' (0 - 49), 'D' (50 - 69), 'C' (70-79), 'B' (80 - 89), 'A' (90 - 100)
- * Результат сохраните в переменной grade.
- */
+const score = 70; 
+let grade;
 
-const score = 70 // тестовое значение, можно изменять
-let grade
-// your code
+if (score >= 90 && score <= 100) {
+    grade = 'A';
+} else if (score >= 80 && score < 90) {
+    grade = 'B';
+} else if (score >= 70 && score < 80) {
+    grade = 'C';
+} else if (score >= 50 && score < 70) {
+    grade = 'D';
+} else if (score >= 0 && score < 50) {
+    grade = 'F';
+} else {
+    grade = 'Неверный балл'; 
+}
+
+console.log(grade);
