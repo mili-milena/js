efc414182b75b0cd7f84533c7777fcde9ca4fdd2
Milena <milenapavlovihc14@gmail.com>
2025-06-21 16:36:46 +0300

    lesson 2/03,2

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,20 +1,16 @@
-// Инициализация переменной score до использования
-var score = 45; // Устанавливаем начальное значение
-
-var grade;
+const score = 45;
+let grade;
 
 if (score >= 90 && score <= 100) {
-    grade = 'A';
+  grade = 'A';
 } else if (score >= 80 && score <= 89) {
-    grade = 'B';
+  grade = 'B';
 } else if (score >= 70 && score <= 79) {
-    grade = 'C';
+  grade = 'C';
 } else if (score >= 50 && score <= 69) {
-    grade = 'D';
+  grade = 'D';
 } else if (score >= 0 && score <= 49) {
-    grade = 'F';
+  grade = 'F';
 } else {
-    grade = 'Invalid';  // Если score выходит за пределы 0-100
+  grade = 'Invalid score';
 }
-
-console.log(grade);  // Выводим результат
