624519f8f5a8ff15b34ef136a280f92d799c3db0
Milena <milenapavlovihc14@gmail.com>
2025-06-21 16:59:26 +0300

    lesson 2/03,2

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -12,7 +12,7 @@
 🧙‍♂️ Совет: обратите внимание на управление индексом текущего изображения — это ключ к успешному переключению изображений.
 */
 
-const WEB_TECH_IMAGES = [
+/*const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
@@ -44,4 +44,4 @@ nextButton.addEventListener('click', () => {
   }
   imgElement.src = WEB_TECH_IMAGES[currentIndex];
 });
-
+*/
