8f732cc60122ceac962377c141962051ea245944
Milena <milenapavlovihc14@gmail.com>
2025-06-21 16:06:59 +0300

    lesson-5/02

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,6 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(gallery, artworkTitle, newValue) {
+  gallery[artworkTitle] = newValue;
+}
