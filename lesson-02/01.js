c1258edf7c186f0fcf1cb23717c440c404b4f5ce
Milena <milenapavlovihc14@gmail.com>
2025-06-21 16:58:09 +0300

    lesson 2/03,2

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,10 +1,17 @@
-const isAdmin = false;
-const isVerifiedUser = true;
-const hasSpecialPermission = true;
-const hasTemporaryPass = false;
+/*const score = 45; // или любое другое число от 0 до 100
+let grade;
 
-let isAccess;
-
-isAccess = (isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass);
-
-console.log(isAccess);
+if (score >= 90 && score <= 100) {
+  grade = 'A';
+} else if (score >= 80 && score <= 89) {
+  grade = 'B';
+} else if (score >= 70 && score <= 79) {
+  grade = 'C';
+} else if (score >= 50 && score <= 69) {
+  grade = 'D';
+} else if (score >= 0 && score <= 49) {
+  grade = 'F';
+} else {
+  grade = 'Invalid score';
+}
+*/
